1976997d7122671f03a184bb9a49f60b1c768b87
Alessio Buccino <alejoe9187@gmail.com>
2026-02-05 10:39:13 +0100

    Update to `aind-data-schema` 2.0 (#1)

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import logging
 import json
 import shutil
@@ -31,13 +32,44 @@ if __name__ == "__main__":
     logging.info(f"Found {len(quality_control_json_files)} quality control files")
     main_qc = None
 
+    recording_names = []
+    all_metrics = []
+    default_grouping = []
+    recording_segments = []
     for quality_control_json_file in quality_control_json_files:
         # json file names are: quality_control_{recording_name}.json
         recording_name = "_".join(quality_control_json_file.name.split("_")[2:])[:-5]
+        recording_names.append(recording_name)
+        if "group" in recording_name:
+            segment_str = recording_name.split("_")[-2]
+        else:
+            segment_str = recording_name.split("_")[-1]
+        recording_segments.append(segment_str)
+
+    drop_recording_segment = len(set(recording_segments)) == 1
+    if drop_recording_segment:
+        logging.info(
+            "All recording segments are the same. Dropping recording segment from recording name."
+        )
+
+    allow_tag_failures = []
+    for quality_control_json_file in quality_control_json_files:
+        # load original QC to retrieve abbreviated recording name (default_grouping)
+        recording_name = "_".join(quality_control_json_file.name.split("_")[2:])[:-5]
+        with open(quality_control_json_file) as f:
+            original_qc_data = json.load(f)
+        original_qc = QualityControl(**original_qc_data)
+        abbrv_recording_name = original_qc.metrics[0].tags["probe"]
+
+        if drop_recording_segment:
+            recording_str = f"_{recording_segments[0]}"
+            new_recording_name = abbrv_recording_name.replace(recording_str, "")
+        else:
+            new_recording_name = abbrv_recording_name
 
         # copy figures
         input_figure_folder = data_folder / f"quality_control_{recording_name}"
-        output_figure_folder = results_folder / "quality_control" / recording_name
+        output_figure_folder = results_folder / "quality_control" / new_recording_name
         output_figure_folder.mkdir(parents=True, exist_ok=True)
 
         for fig_file in input_figure_folder.iterdir():
@@ -46,27 +78,29 @@ if __name__ == "__main__":
 
         # read json and remap reference strings
         qc_json_str = json.dumps(json.load(open(quality_control_json_file)))
-        qc_json_str = qc_json_str.replace(f"quality_control_{recording_name}", f"quality_control/{recording_name}")
+        qc_json_str = qc_json_str.replace(f"quality_control_{recording_name}", f"quality_control/{new_recording_name}")
+        if drop_recording_segment:
+            # replace all segment entries except when segment string is in a zarr paths
+            qc_json_str = re.sub(rf"{re.escape(recording_str)}(?!(?:(?!/).)*\.zarr)", "", qc_json_str)
 
         # load qc and append evaluations
         qc = QualityControl(**json.loads(qc_json_str))
-        if main_qc is None:
-            main_qc = qc
-        else:
-            main_eval_names = [ev.name for ev in main_qc.evaluations]
-            for ev in qc.evaluations:
-                if ev.name in main_eval_names:
-                    eval_index = main_eval_names.index(ev.name)
-                    main_qc.evaluations[eval_index].metrics.extend(ev.metrics)
-                else:
-                    main_qc.evaluations.append(ev)
+        all_metrics.extend(qc.metrics)
+        allow_tag_failures.extend(qc.allow_tag_failures)
 
-    # write final quality_metrics.json
-    for ev in main_qc.evaluations:
-        logging.info(f"\tCollected {len(ev.metrics)} metrics for '{ev.name}' evaluation")
+    # create main QC with all metrics and detault tags
+    logging.info(
+        f"\tCollected {len(all_metrics)} metrics for {len(default_grouping)} tags and {len(recording_names)} streams."
+    )
+    logging.info(f"\tTags allowed to fail: {allow_tag_failures}")
 
-    with (results_folder / f"quality_control.json").open("w") as f:
-        f.write(main_qc.model_dump_json(indent=3))
+    main_qc = QualityControl(
+        metrics=all_metrics,
+        default_grouping=[("probe"), ("stage")],
+        allow_tag_failures=allow_tag_failures
+    )
+    
+    main_qc.write_standard_file(output_directory=results_folder)
 
     t_qc_end_all = time.perf_counter()
     elapsed_time_qc_all = np.round(t_qc_end_all - t_qc_start_all, 2)
